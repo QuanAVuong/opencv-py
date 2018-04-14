7afbeca4f92a72b418a9fa69a7e253b17e5c0baf
Quan <QuanAVuong@users.noreply.github.com>
2018-04-14 07:25:08 -0400

    load and store image

--- 2.4-read, write, display images.py
+++ 2.4-read, write, display images.py
@@ -1,2 +1,6 @@
 import cv2
 import numpy as np
+
+# https://docs.opencv.org/2.4/modules/highgui/doc/reading_and_writing_images_and_video.html?highlight=imread#imread
+# cv2.imread(filename[, flags]) → retval
+image = cv2.imread("./images/Computer-Vision.jpg")
