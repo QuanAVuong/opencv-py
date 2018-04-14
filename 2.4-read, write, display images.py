492c08c0fc7cfbee0810ef5762f1338a0de26df5
Quan <QuanAVuong@users.noreply.github.com>
2018-04-14 08:35:20 -0400

    shape() => stored image array's dimension: (height, width, RGB)

--- 2.4-read, write, display images.py
+++ 2.4-read, write, display images.py
@@ -3,13 +3,13 @@ import numpy as np
 
 # https://docs.opencv.org/2.4/modules/highgui/doc/reading_and_writing_images_and_video.html?highlight=imread#imread
 # cv2.imread(filename[, flags]) -> retval
-image = cv2.imread("./images/Gods Eye Graphics by Cantina.jpg")
+input = cv2.imread("./images/Gods Eye Graphics by Cantina.jpg")
 
 # https://docs.opencv.org/2.4/modules/highgui/doc/user_interface.html#void%20imshow(const%20string&%20winname,%20InputArray%20mat)
 # cv2.imshow(winname, mat) -> None
 # winname: Name of the window.
 # image: Image to be shown.
-cv2.imshow("Computer Visions", image)
+cv2.imshow("Computer Visions", input)
 
 # https://docs.opencv.org/2.4/modules/highgui/doc/user_interface.html?highlight=waitkey#waitkey
 # Waits for a pressed key, allows us to input information when a image window is open
@@ -22,3 +22,8 @@ cv2.waitKey(0)
 # The function destroyAllWindows destroys all of the opened HighGUI windows.
 # Close all window, else program hangs
 cv2.destroyAllWindows()
+
+
+print(input.shape)		# => (417L, 1000L, 3L)
+print 'Height of Image:', int(input.shape[0]), 'pixels'		# => Height of Image: 417 pixels
+print 'Width of Image: ', int(input.shape[1]), 'pixels'		# => Width of Image:  1000 pixels
