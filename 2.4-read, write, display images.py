f752ccde0163b07fc03563019236b05a55c0130c
Quan <QuanAVuong@users.noreply.github.com>
2018-04-14 07:55:20 -0400

    delete non-ASCII characters

--- 2.4-read, write, display images.py
+++ 2.4-read, write, display images.py
@@ -2,23 +2,23 @@ import cv2
 import numpy as np
 
 # https://docs.opencv.org/2.4/modules/highgui/doc/reading_and_writing_images_and_video.html?highlight=imread#imread
-# cv2.imread(filename[, flags]) → retval
-image = cv2.imread("./images/Computer-Vision.jpg")
+# cv2.imread(filename[, flags]) -> retval
+image = cv2.imread("./images/Gods Eye Graphics by Cantina.jpg")
 
 # https://docs.opencv.org/2.4/modules/highgui/doc/user_interface.html#void%20imshow(const%20string&%20winname,%20InputArray%20mat)
-# cv2.imshow(winname, mat) → None
-# winname – Name of the window.
-# image – Image to be shown.
+# cv2.imshow(winname, mat) -> None
+# winname: Name of the window.
+# image: Image to be shown.
 cv2.imshow("Computer Visions", image)
 
 # https://docs.opencv.org/2.4/modules/highgui/doc/user_interface.html?highlight=waitkey#waitkey
 # Waits for a pressed key, allows us to input information when a image window is open
-# cv2.waitKey([delay]) → retval¶
-# delay – Delay in milliseconds. 0 is the special value that means “forever”.
-cv2.waitKey()
+# cv2.waitKey([delay]) -> retval
+# delay: Delay in milliseconds. 0 is the special value that means "forever".
+cv2.waitKey(0)
 
 # https://docs.opencv.org/2.4/modules/highgui/doc/user_interface.html#void%20destroyAllWindows()
-# cv2.destroyAllWindows() → None
+# cv2.destroyAllWindows() -> None
 # The function destroyAllWindows destroys all of the opened HighGUI windows.
 # Close all window, else program hangs
 cv2.destroyAllWindows()
